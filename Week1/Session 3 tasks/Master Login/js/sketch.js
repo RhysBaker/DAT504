e148ff2d618042d57299af5805c9c1221b1065fb
Rhys Baker <rhys.baker99@gmail.com>
2018-09-27 15:13:27 +0100

    week 1 work

---
+++ Week1/Session 3 tasks/Master Login/js/sketch.js
@@ -0,0 +1,25 @@
+var login = prompt('Please login');
+
+if (login == null) {
+  document.getElementById('welcome').innerHTML = 'Hello ' + 'Invalid User' + ', I dont know you.';
+}
+
+if (login != null) {
+  document.getElementById('welcome').innerHTML = 'Hello ' + login + ', get a better name.';
+}
+
+if (login == 'admin') {
+  var adminPass = prompt('Please enter the password');
+  if (adminPass == '123') {
+    document.getElementById('welcome').innerHTML = 'Hello, Master ' + login;
+  }
+
+  while (adminPass != '123') {
+    if (adminPass == null) {
+      document.getElementById('welcome').innerHTML = 'Why did you cancel?';
+      break;
+    }
+    var adminPass = prompt('Wrong password, try again.');
+    document.getElementById('welcome').innerHTML = 'Hello, Master ' + login;
+  }
+}
