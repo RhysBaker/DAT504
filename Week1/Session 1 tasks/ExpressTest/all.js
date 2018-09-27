4b791822573d7a6112dbea0cfcb94d8c9e6df42c
Rhys Baker <rhys.baker99@gmail.com>
2018-09-27 17:24:20 +0100

    adding post to add new course to array

--- Week1/Session 1 tasks/ExpressTest/all.js
+++ Week1/Session 1 tasks/ExpressTest/all.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
+
 const courses  = [
   { id: 1, name: 'course1' },
   { id: 2, name: 'course2' },
@@ -19,9 +19,11 @@ app.get('/api/courses', (req, res) => {
 
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send('The course with the givin id was');
+  if (!course) res.status(404).send('The course with the givin id was not valid');
   res.send(course);
 });
+
+app.use(express.json());
 
 app.post('/api/courses', (req, res) => {
   const course = {
